b0d2b16fa1a92ba4c23e635f5ed3be1a79e828fd
Vu Hoang Vu <23162116@student.hcmute.edu.vn>
2024-10-24 14:30:44 +0700

    Diem so

---
+++ Class DiemSo/Source.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+pygame.init()
+
+# Hang so cho cua so tro choi
+RONG_CUA_SO = 600
+CAO_CUA_SO = 400
+KICH_THUOC_O = 20
+
+# Mau sac
+DEN = (0, 0, 0)
+TRANG = (255, 255, 255)
+XANH_LA = (0, 255, 0)
+DO = (255, 0, 0)
+
+# Viet tiep code tu day tro xuong
+
+class DiemSo:
+    def __init__(self):
+        self.diem_hien_tai = 0
+    
+    def cap_nhat_diem(self):
+
+    def hien_thi_diem(self, cua_so):
+    
+    
